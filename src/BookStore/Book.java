6ec8b5511c8e11ebbe7208617f6e63abe65e0c62
Aradeid <mcurmei@mail.ru>
2022-10-06 10:58:43 +0300

    Added initial functions

---
+++ src/BookStore/Book.java
@@ -0,0 +1,47 @@
+package BookStore;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Book {
+	private String title;
+	private List<String> images = new ArrayList<>();
+	private List<String> paragraphs = new ArrayList<>();
+	private List<String> tables = new ArrayList<>();
+
+	public Book(String title) {
+		this.title = title;
+	}
+
+	public boolean createNewParagraph(String paragraph) {
+		return this.images.add(paragraph);
+		
+	}
+
+	public boolean createNewImage(String image) {
+		return this.images.add(image);
+	}
+
+	public boolean createNewTable(String table) {
+		return this.tables.add(table);
+		
+	}
+
+	public void print() {
+		System.out.println("Title: " + title);
+		System.out.println("Paragraphs:");
+		for(String p : paragraphs) {
+			System.out.println(p);
+		}
+		
+		System.out.println("Images:");
+		for(String im : images) {
+			System.out.println(im);
+		}
+		
+		System.out.println("Tables:");
+		for(String t : tables) {
+			System.out.println(t);
+		}
+	}
+}
